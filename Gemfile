ae359bbb50d8a0653c88c2dd9bf5eaa082dbf3b4
Zeke Sikelianos <zeke@sikelianos.com>
2011-01-08 20:29:31 -0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem "compass", ">= 0.10.6"
+gem "haml", ">= 3.0.25"
+gem "configatron", ">= 2.6.4"
+gem 'jquery-rails', '>= 0.2.6'
+gem 'formtastic', '~> 1.2.2'
+gem 'httparty', '>= 0.6.1'
+gem 'common_view_helpers', '>= 0.3.1'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+group :development, :test do
+  gem "rspec", "2.4.0"
+  gem "rspec-rails", "2.4.1"
+  gem "cucumber-rails", ">= 0.3.2"
+  gem "webrat", ">= 0.7.3"
+  gem 'factory_girl_rails', ">= 1.0.1"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordPong::Application.config.secret_token = 'f7a67561312703b3b638cdb18b77d21f696e55a9d38acb2ad8da9cd076ab019d464cc388cd7c0be09b7296b18703d523c2cc528a5bfdbd267584ee56e6f4bb7b'

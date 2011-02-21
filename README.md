f77692a0541870bb09152934a258e7a8cfb7b119
Zeke Sikelianos <zeke@sikelianos.com>
2011-02-20 18:03:56 -0800

    Added a 'Rhyme' play mode and updated the README

---
+++ README.md
@@ -0,0 +1,13 @@
+WordPong
+========
+
+A word association game, powered by the [Wordnik API](http://developer.wordnik.com)
+
+Go play at [wordpong.heroku.com](http://wordpong.heroku.com)
+
+To Do
+-----
+
+* When resorting to a random word response, ensure the random word is an adjective
+* Keep Pings within a certain level of accessibility (corpus frequency)
+* Add an "I Don't Know" option
--- public/javascripts/configatron.js
+++ public/javascripts/configatron.js
@@ -1,6 +1,6 @@
 
 function configatron(key) {
-	var config = {"javascripts":["https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js","rails","jquery.ezCookie_0.7.01","configatron","application"],"api":{"key":"b39ee8d5f05d0f566a0080b4c310ceddf5dc5f7606a616f53","base_path":"http://beta.wordnik.com/api/"},"desirable_relation_types":["antonym","same-context","synonym"],"mode_keys":["antonymy","synonymy","free_association"],"modes":{"antonymy":"is the opposite of","synonymy":"is another word for","free_association":"makes me think of"}};
+	var config = {"javascripts":["https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js","rails","jquery.ezCookie_0.7.01","configatron","application"],"api":{"key":"b39ee8d5f05d0f566a0080b4c310ceddf5dc5f7606a616f53","base_path":"http://beta.wordnik.com/api/"},"desirable_relation_types":["antonym","same-context","synonym"],"mode_keys":["antonymy","synonymy","rhyme","free_association"],"modes":{"antonymy":"is the opposite of","synonymy":"is another word for","rhyme":"rhymes with","free_association":"makes me think of"}};
 	var keys = key.split('.');
 	var obj = config;
 	for (var i=0; i<keys.length; i++) { obj = obj[keys[i]] };
